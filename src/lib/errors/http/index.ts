63d4d61521d97daa38634d038db085b089f320b3
Michael Bondarevsky <bondamc@gmail.com>
2023-10-27 22:01:48 +0300

    Remove code from index files

--- src/lib/errors/http/index.ts
+++ src/lib/errors/http/index.ts
@@ -1,12 +1,2 @@
 // Internal.
-import { RawNetworkingError } from '../networking-error';
-import { HttpError } from './http-error';
-
-export function httpErrorFactory(err: RawNetworkingError): HttpError {
-    if (err instanceof TypeError) {
-        // Non-retryable.
-        return new HttpError(err, false);
-    }
-    // By default return a retryable HTTP error.
-    return new HttpError(err, true);
-}
+export { httpErrorFactory } from './factory';
