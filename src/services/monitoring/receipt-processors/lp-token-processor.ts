07e53000cbb9ba681b5ca4532b69d92718ea39ab
Michael Bondarevsky <bondamc@gmail.com>
2023-11-24 15:23:09 +0200

    Refactor

--- src/services/monitoring/receipt-processors/lp-token-processor.ts
+++ src/services/monitoring/receipt-processors/lp-token-processor.ts
@@ -58,10 +58,10 @@ export class LPTokenReceiptProcessor extends BaseProcessor {
             this.getNewERC20(token2Addr),
         ]);
         const newToken = token1 || token2;
+        this.logger.info('Liquidity pool created', { pool, tracked: !!newToken });
         if (newToken) {
             await this.saveTrackedToken(newToken.address);
             await this.deleteNewERC20(newToken.address);
-            this.logger.info('Liquidity pool created for tracked token', { pool });
         }
     }
 
